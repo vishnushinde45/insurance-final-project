70c4e7723afb8cbf47516ed0cccc180d528630b6
vishnushinde45 <vshinde13798@gmail.com>
2022-10-31 16:32:11 +0530

    d

---
+++ src/app/admin/admin-login/admin-login.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit,Input,Output,EventEmitter} from '@angular/core';
+import { Router } from '@angular/router';
+import {FormBuilder,FormGroup,FormControl,Validators} from '@angular/forms';
+import { AdminService } from '../admin.service';
+
+
+@Component({
+  selector: 'app-admin-login',
+  templateUrl: './admin-login.component.html',
+  styleUrls: ['./admin-login.component.css']
+})
+export class AdminLoginComponent implements OnInit {
+
+  constructor(private formBuilder:FormBuilder,private adminService:AdminService,private router:Router) { }
+
+  adminLoginDetails=this.formBuilder.group({
+    username:new FormControl(),
+    password:new FormControl(),
+    
+  });
+ 
+  ngOnInit(): void {
+  }
+
+  responseData:any;
+  onLoginClick(){
+   this.adminService.adminLogin(this.adminLoginDetails.value).subscribe((res)=>{
+      this.responseData=res;
+      this.router.navigate(['admin/dashboard'],{ queryParams: { 'id': this.responseData.id } })
+      sessionStorage.setItem('username',this.responseData.username);
+   },((err:any)=>{
+              console.log(err);
+              
+   }));
+
+   
+}}
